b8d464a03198b00c626247154bb5a4a2e21d641a
Luke Donahue <luke9389@gmail.com>
2019-09-23 13:19:33 -0700

    combined w lab-03 repo and starter code

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+// const DocumentCollection = require('./lib/document-collection');
 const validator = require('./lib/validator.js');
 
 console.log(validator.isString('hello world'));
+
+// const documents = new DocumentCollection(/* path to use */);
